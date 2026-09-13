8a4f64b21f59da8767ad61d9b97cead681bc70d4
Maksym Pravoslavnyi <malexxbtc@gmail.com>
2026-09-13 15:02:14 +0300

    Move project 3_0 inside EMB_hw-2_4 directory

--- 3_0/main/hardware.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "esp_err.h"
-#include "esp_adc/adc_oneshot.h"
-
-esp_err_t hardware_init(adc_oneshot_unit_handle_t *adc_handle);
-int hardware_adc_read(adc_oneshot_unit_handle_t adc_handle);
-void hardware_led_set(uint8_t state);
